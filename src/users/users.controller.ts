1870ec13d9738030709f4cc588274636c12d42e1
Trifonix <trifonix@rocketmail.com>
2024-06-05 17:11:35 +0300

    init work 03-04_06_2024

---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    // Добавляем список пользователей
+    users = [
+        {
+            id: 1,
+            name: 'User 1'
+        },
+        {
+            id: 2,
+            name: 'User 2'
+        }
+    ];
+
+    @Get()
+    getAll(): { name: string; id: number; }[] {
+        return this.users;
+    }
+
+    @Get(':id')
+    findOne(
+        @Param('id', ParseIntPipe) id: number
+    ): { name: string; id: number } | null {
+        return this.users.find((user) => user.id === id);
+    }
+}
+
