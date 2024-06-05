7e9a1f5eee8b19e7e4c8acaa7758fb6063650ff0
Trifonix <trifonix@rocketmail.com>
2024-06-05 17:41:02 +0300

    add cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
+  controllers: [AppController, UsersController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
